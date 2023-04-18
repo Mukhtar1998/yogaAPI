0c0da2e7febbb372e343314eb461aa07f13ef555
Mukhtar1998 <94168491+Mukhtar1998@users.noreply.github.com>
2023-04-18 11:00:08 +0200

    :art: installing the dependencies and adding user

---
+++ routes/UserRoute.js
@@ -0,0 +1,88 @@
+import { Router } from "express";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// 1 -crypte password
+// 2 -hache password
+const userRouter = Router();
+
+userRouter.post("/register", async (req, res) => {
+	try {
+		const userExist = await User.findOne({ email: req.body.email });
+		if (userExist) {
+			return res
+				.status(200)
+				.json({ message: "user already exists", success: false });
+		}
+
+		const password = req.body.password;
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		req.body.password = hashedPassword;
+		const newUser = new User(req.body);
+		await newUser.save();
+		res.status(200).json({
+			message: "user created successfully",
+			success: true,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			message: "error creating user",
+			success: false,
+			error,
+		});
+	}
+});
+
+userRouter.post("/login", async (req, res) => {
+	try {
+		const user = await User.findOne({ email: req.body.email });
+		if (!user)
+			return res
+				.status(200)
+				.json({ message: "user does not exist", success: false });
+		const isMatch = await bcrypt.compare(req.body.password, user.password);
+		if (!isMatch)
+			return res
+				.status(200)
+				.json({ message: "password does not match", success: false });
+		else {
+			const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+				expiresIn: "1d",
+			});
+			res
+				.status(200)
+				.json({ message: "login succesfull", success: true, data: token });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "error login", success: false, error });
+	}
+});
+
+userRouter.get("/users/:useremail", async (req, res) => {
+	try {
+		const userExist = await User.findOne({ email: req.params.useremail });
+		if (userExist) {
+			return res
+				.status(200)
+				.json({ message: "user already exists", success: false });
+		}else {
+			return res
+			.status(404)
+				.json({ message: "user not exist", success: false });
+		}
+
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			message: "error getting user",
+			success: false,
+			error,
+		});
+	}
+});
+
+export default userRouter;
---
+++ server.js
@@ -0,0 +1,16 @@
+import userRouter from "./routes/UserRoute.js";
+// import mongoose from "../config/dbConfig.js";
+import express from "express";
+const app = express();
+app.use(express.json());
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+app.use(express.urlencoded({ extended: false }));
+
+const port = process.env.PORT;
+app.use("/api/users", userRouter);
+app.listen(port, () => console.log(`application is running on port ${port}`));
+// http://localhost:3000/login --> front + data
+// http://localhost:4000/api/users/login --> BACK + data nv
